6d3b04e555a2df5f0507c86d33931a4e27eb7b0f
FabioVascão <fabiosousa2691@hotmail.com>
2023-01-10 14:30:02 -0300

    inicio

---
+++ Desafio-Front-End/src/styles/theme.js
@@ -0,0 +1,19 @@
+export default {
+    COLORS: {
+      BACKGROUND_DARK: "#1C1B1E",
+      BACKGROUND_MEDIUM: "#262529",
+      BACKGROUND_LIGHT: "#3E3B47",
+      BACKGROUND_BUTTON: "#FF859B",
+      BACKGROUND_TAGS: "#312E38",
+  
+      WHITE: "#F4EDE8",
+      BLACK: "000000",
+      PINK: "#FF859B",
+  
+      GRAY_100: "#CAC4CF",
+      GRAY_300: "#948F99",
+      GRAY_500: "#999591",
+      GRAY_700: "#262529"
+  
+    }
+  }
